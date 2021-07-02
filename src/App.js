0cb14f3da97e32f9f8d61afc9f391dd777b6513d
Mohammed Rayan sailani <rayanbackup1@gmail.com>
2021-07-02 20:08:18 +0530

    first push

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { useState } from "react";
+import AddUser from "./Components/Users/AddUser";
+
+
+function App() {
+  // const [inputState, setInputState] = useState([]);
+  return (
+    <div>
+      <AddUser />
+    </div>
+  );
+}
+
+export default App;
